29b16f35816e7dfb3b76e7f30cb908ed39ad4255
krysros <krystian.rosinski@gmail.com>
2023-08-26 11:08:56 +0200

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# surf_grid
-Surface reconstruction from scattered data points
+# README
+
+Surface reconstruction from scattered data points.
+
+The input data is read from the dxf file. On their basis, interpolation of surfaces (e.g. terrain surfaces) is performed. The results in the form of a grid of squares are saved in a dxf file on separate layers and a csv file. These data can be used to calculate the volume of earthworks using the square method. You can also visualize the reconstructed surface using a [skar.py](https://github.com/krysros/skarpy.git).
+
+## Installation
+
+```console
+python -m pip install -r requirements.txt
+```
+
+## Calculations
+
+```console
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,82 @@
+import ezdxf
+import numpy as np
+from ezdxf import zoom
+from ezdxf.enums import TextEntityAlignment
+from scipy.interpolate import griddata
+
+doc = ezdxf.readfile("map.dxf")
+msp = doc.modelspace()
+circles = msp.query('CIRCLE[layer=="pts"]')
+
+doc.header["$PDMODE"] = 32
+doc.header["$PDSIZE"] = 0.25
+
+doc.styles.new("myStandard", dxfattribs={"font": "Arial.ttf"})
+
+doc.layers.new(name="sqrs", dxfattribs={"color": 4})
+doc.layers.new(name="grid", dxfattribs={"color": 6})
+
+coords = []
+
+for circle in circles:
+    x, y, z = circle.dxf.center
+    p = (x, y, z)
+    coords.append(p)
+    msp.add_circle(p, 0.5)
+    msp.add_text(f"{z}", height=0.5, dxfattribs={"style": "myStandard"}).set_placement(
+        (x, y), align=TextEntityAlignment.LEFT
+    )
+
+coords = np.array(coords)
+
+points = coords[:, :2]
+values = coords[:, 2]
+
+min_x = np.min(coords[:, 0])
+max_x = np.max(coords[:, 0])
+min_y = np.min(coords[:, 1])
+max_y = np.max(coords[:, 1])
+
+delta_x = max_x - min_x
+delta_y = max_y - min_y
+
+step = 2
+delta_x = np.around(delta_x / step)
+delta_y = np.around(delta_y / step)
+
+grid_x = np.arange(0, delta_x) * step + min_x
+grid_y = np.arange(0, delta_y) * step + min_y
+
+X, Y = np.meshgrid(grid_x, grid_y)
+Z = griddata(points, values, (X, Y), method="linear")
+Z = Z.T
+
+icoords = []
+num = 0
+data = []
+
+for i, x in enumerate(grid_x):
+    for j, y in enumerate(grid_y):
+        num += 1
+        msp.add_text(
+            f"{num}", height=0.25, dxfattribs={"layer": "sqrs", "style": "myStandard"}
+        ).set_placement((x + step / 2, y + step / 2), align=TextEntityAlignment.CENTER)
+        z = Z[i, j]
+        try:
+            data.append([num, Z[i, j], Z[i + 1, j], Z[i, j + 1], Z[i + 1, j + 1]])
+        except IndexError:
+            pass
+        if not np.isnan(z):
+            msp.add_point((x, y, z), dxfattribs={"layer": "grid"})
+
+data = np.array(data)
+np.savetxt(
+    "output.csv",
+    data,
+    delimiter=";",
+    fmt=["%i", "%10.4f", "%10.4f", "%10.4f", "%10.4f"],
+    encoding="utf-8",
+)
+
+zoom.extents(msp)
+doc.saveas("output.dxf")
